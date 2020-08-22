c9f3f2498be964eb2d926af8333a6d26cca9936a
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-08-21 22:02:30 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM docker.io/nodisbr/python
+
+RUN apt-get upgrade
+RUN apt-get -y install git
+
+RUN curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+UPGRADE=()
+PRE_UNINSTALL=()
+UNINSTALL=()
+
+COMMIT_ROLLBACK_COUNT=20
+TEMP_DIR=./temp
+MANIFEST_EXT=yaml
+DEFAULT_IFS=${IFS}
+
+pip install -i "https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple" maestro
+
+LAST_COMMIT=`git log -1 --skip=1 --invert-grep --grep=skip_ci --format=oneline | awk '{print $1}'`
+
+IFS=$'\n'
+for LINE in `git diff --name-status -C ${LAST_COMMIT} HEAD | egrep '.*\/.*'`;  do
+
+    echo ${LINE}
+
+    OPERATION=`echo ${LINE} | awk '{print $1}'`
+
+    FILE=`echo ${LINE} | awk '{print $2}'`
+    FILE_EXT=${FILE##*.}
+
+    if [[ ${FILE_EXT} =~ ${MANIFEST_EXT} ]]; then
+
+        [[ ${OPERATION} =~ (A|M) ]] && UPGRADE+=(${FILE})
+        [[ ${OPERATION} = "D" ]] && PRE_UNINSTALL+=(${FILE})
+
+    fi
+
+    if [[ ${OPERATION} =~ ^(R|C) ]]; then
+
+        NEW_FILE=`echo ${LINE} | awk '{print $3}'`
+        NEW_FILE_EXT=${NEW_FILE##*.}
+
+        [[ ${NEW_FILE_EXT} =~ ${MANIFEST_EXT} ]] && UPGRADE+=(${NEW_FILE})
+        [[ ${OPERATION} =~ ^R && ${FILE_EXT} =~ ${MANIFEST_EXT} && ${NEW_FILE_EXT} != ${FILE_EXT} ]] && PRE_UNINSTALL+=(${NEW_FILE})
+
+    fi
+
+done
+IFS=${DEFAULT_IFS}
+
+
+[[ ${#UPGRADE[@]} -gt 0 ]] && echo "maestro -u upgrade ${MAESTRO_OPTIONS} -f ${UPGRADE[@]}"
+
+if [[ ${#PRE_UNINSTALL[@]} -gt 0 ]]; then
+
+    mkdir -p ${TEMP_DIR}
+
+    for F in ${PRE_UNINSTALL[@]}; do
+
+        if ! [[ -f ${F} ]]; then
+            IFS=$'\n'
+            for COMMIT in `git log -${COMMIT_ROLLBACK_COUNT} --skip=1 --format=oneline | awk '{print $1;}'`; do
+
+                if git show ${COMMIT}:${F} 2> /dev/null | install -D /dev/stdin ${TEMP_DIR}/${F}; then
+                    UNINSTALL+=("${TEMP_DIR}/${F}")
+                    break
+                fi
+            done
+            IFS=${DEFAULT_IFS}
+        else
+            UNINSTALL+=(${F})
+        fi
+
+    done
+
+    echo "maestro -u uninstall ${MAESTRO_OPTIONS} -f ${UNINSTALL[@]}"
+
+    rm -rf ${TEMP_DIR}
+
+fi
